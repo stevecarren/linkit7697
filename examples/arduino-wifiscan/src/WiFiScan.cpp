34b19a23749c99927ed2a189e3a8f51dd358f8b6
steve <steve.carren@gmail.com>
2019-09-17 21:08:12 -0400

    Fix wifi scan example

--- examples/arduino-wifiscan/src/WiFiScan.cpp
+++ examples/arduino-wifiscan/src/WiFiScan.cpp
@@ -1,46 +1,64 @@
-/*
- *  This sketch demonstrates how to scan WiFi networks.
- *  The API is almost the same as with the WiFi Shield library,
- *  the most obvious difference being the different file you need to include:
- */
-#include "WiFi.h"
+#include "Arduino.h"
+#include "LWiFi.h"
+
+static void printEncryptionType(int thisType)
+{
+    // read the encryption type and print out the name:
+    switch (thisType)
+    {
+    case ENC_TYPE_WEP:
+        Serial.println("WEP");
+        break;
+    case ENC_TYPE_TKIP:
+        Serial.println("WPA");
+        break;
+    case ENC_TYPE_NONE:
+        Serial.println("None");
+        break;
+    case ENC_TYPE_CCMP:
+        Serial.println("WPA2 (AES)");
+        break;
+    case ENC_TYPE_TKIP_AES:
+        Serial.println("TKIP AES MIX");
+        break;
+    default:
+        Serial.println("Unknown");
+    }
+}
 
 void setup()
 {
     Serial.begin(115200);
-
-    // Set WiFi to station mode and disconnect from an AP if it was previously connected
-    WiFi.mode(WIFI_STA);
-    WiFi.disconnect();
-    delay(100);
-
-    Serial.println("Setup done");
 }
 
 void loop()
 {
-    Serial.println("scan start");
-
-    // WiFi.scanNetworks will return the number of networks found
-    int n = WiFi.scanNetworks();
-    Serial.println("scan done");
-    if (n == 0) {
-        Serial.println("no networks found");
-    } else {
-        Serial.print(n);
-        Serial.println(" networks found");
-        for (int i = 0; i < n; ++i) {
-            // Print SSID and RSSI for each network found
-            Serial.print(i + 1);
-            Serial.print(": ");
-            Serial.print(WiFi.SSID(i));
-            Serial.print(" (");
-            Serial.print(WiFi.RSSI(i));
-            Serial.print(")");
-            Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
-            delay(10);
-        }
+    int numSsid = WiFi.scanNetworks();
+    if (numSsid == -1)
+    {
+        Serial.println("Couldn't get a wifi connection");
+        while (true)
+            ;
     }
+
+    // print the list of networks seen:
+    Serial.print("number of available networks:");
+    Serial.println(numSsid);
+
+    // print the network number and name for each network found:
+    for (int thisNet = 0; thisNet < numSsid; thisNet++)
+    {
+        Serial.print(thisNet);
+        Serial.print(") ");
+        Serial.print(WiFi.SSID(thisNet));
+        Serial.print("\tSignal: ");
+        Serial.print(WiFi.RSSI(thisNet));
+        Serial.print(" dBm");
+        Serial.print("\tEncryption: ");
+        int encr = WiFi.encryptionType(thisNet);
+        printEncryptionType(encr);
+    }
+
     Serial.println("");
 
     // Wait a bit before scanning again
